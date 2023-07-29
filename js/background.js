8cb59826a08357c8a02a049b3658bde5daaffede
ponyoung <icepower1234@naver.com>
2023-07-29 18:16:47 +0900

    Clock, Quotes , Bg added

---
+++ js/background.js
@@ -0,0 +1,16 @@
+const images = [
+  "aurora.jpg",
+  "ice.jpg",
+  "penguins.jpg",
+  "rock.jpg",
+  "sky.jpg",
+  "space.jpg",
+  "waterfall.jpg",
+];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
