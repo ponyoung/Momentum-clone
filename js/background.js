6ddb3a80366ed96eb9c5971690148c7858f3311d
ponyoung <icepower1234@naver.com>
2023-08-03 22:45:25 +0900

    CSSing

--- js/background.js
+++ js/background.js
@@ -12,5 +12,6 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
 bgImage.src = `img/${chosenImage}`;
+bgImage.id = "bg";
 
 document.body.appendChild(bgImage);
---
+++ js/weather.js
@@ -0,0 +1,21 @@
+const API_KEY = "d986f96427b8be79086986b30bc65e93";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError() {
+  alert("Can't find you. No weather for you. ㅋ");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
